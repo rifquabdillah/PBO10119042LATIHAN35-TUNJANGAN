f80ed2ee2250c5b4097664ff9d0a6d1d8cb166e7
rifquabdillah <72897695+rifquabdillah@users.noreply.github.com>
2020-11-06 20:27:38 +0700

    Add files via upload

---
+++ Tunjangan.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo;
+import java.util.Scanner;
+/**
+ *
+ * @author Muhammad RIfqu Abdillah
+ *  Nama : Muhammad Rifqu Abdillah
+ *  Kelas: Pbo-IF1
+ *  Nim : 10119042 
+ */
+public class Tunjangan {
+    public static String status;
+    public static Double gaji,tunjangan;
+   
+    public static void inputData(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("=====Program Tunjangan=====");
+        System.out.print("Berapa Gaji Pokok Anda perBulan? : Rp. ");
+        gaji = scanner.nextDouble();
+        
+        System.out.print("Status Anda?(Menikah/Belum\t) : ");
+        status = scanner.next();
+        while (!(status.toUpperCase().equals("MENIKAH") || status.toUpperCase().equals("BELUM"))){
+            System.out.print("Status Anda?(Menikah/Belum)\t: ");
+            status = scanner.next();
+        }
+    }
+     public static void cekStatus(){
+        tunjangan = (status.toUpperCase().equals("MENIKAH"))? hitungTunjangan(gaji):0.0;
+    }
+    
+     public static double hitungTunjangan(double gaji){
+     double hitungTunjangan = 0.35 * gaji;
+     return hitungTunjangan;
+     }
+     public static void tampilFormat(){
+         System.out.println("=====Hasil Perhitungan Gaji Anda=====");
+         System.out.println("Gaji Pokok\t\t\t: Rp. " + gaji);
+         System.out.printf("Tunjangan\t\t\t: Rp. %.1f%n", tunjangan);
+         double totalGaji = gaji + tunjangan;
+         System.out.println("Total Gahu\t\t\t: Rp. " + totalGaji);
+         
+         System.out.println("Developed by : M.Rifqu Abdillah");
+     }
+    
+    
+}
